e52165ea7c9c0de4070de526f3136909fd96e370
Owenchiu729 <112868814+Owenchiu729@users.noreply.github.com>
2025-02-14 00:24:32 +0800

    Delete C語言教學手冊Sorce_Code directory

--- C語言教學手冊Sorce_Code/Prog4_2.C
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-int main (void)
-{
-printf("Have a nice day!!\n"); /*©I¥sprintf¨ç¼Æ*/
-system("pause");
-return 0;
-}
--- C語言教學手冊Sorce_Code/Prog4_3.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-int main (void)
-{
-int num=25;
-printf("\"%d%%的學生來自小康家庭\"\n", num); /*呼叫printf函數*/
-system("pause");
-return 0;
-}
