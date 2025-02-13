277a9422473c35fa41457cb24f13574d4c400e23
Owenchiu729 <112868814+Owenchiu729@users.noreply.github.com>
2025-02-14 00:27:34 +0800

    Add files via upload

---
+++ C語言教學手冊Sorce_Code/第四章Code/Prog4_1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main (void)
+{
+int a=2;
+int b=4;
+printf("i have %d dogs and %d cats\n",a,b); /*©I¥sprintf¨ç¼Æ*/
+system("pause");
+return 0;
+}
---
+++ C語言教學手冊Sorce_Code/第四章Code/Prog4_7.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(void)
+{
+int a=15;
+printf("a=%d\n",a);   /*Print the value of a*/
+printf("float print: %f\n",(float)a); /*Print the float value of a*/
+printf("Print in index format: %e\n",(float)a);/*Print in index format of a*/
+    getchar(); 
+    return 0;
+
+}
